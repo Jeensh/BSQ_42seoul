fe046cb392b9f45797a95cd1f0f722763b40e03b
donghshi <donghshi@student.42seoul.kr>
2022-02-23 12:00:53 +0900

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: donghshi <donghshi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 10:14:55 by donghshi          #+#    #+#             */
-/*   Updated: 2022/02/23 09:45:42 by donghshi         ###   ########.fr       */
+/*   Updated: 2022/02/23 11:48:20 by donghshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "my_lib.h"
@@ -32,8 +32,6 @@ int	main(int argc, char *argv[])
 		while (++i < argc)
 		{
 			get_solution(argv[i]);
-			if (i != (argc - 1))
-				write(1, "\n", 1);
 		}
 	}
 //	system("leaks bsq");
--- my_print.c
+++ my_print.c
@@ -6,7 +6,7 @@
 /*   By: donghshi <donghshi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 10:12:13 by donghshi          #+#    #+#             */
-/*   Updated: 2022/02/22 18:45:03 by donghshi         ###   ########.fr       */
+/*   Updated: 2022/02/23 11:50:12 by donghshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,5 @@ void	print_result(char **result, t_opt *c)
 
 void	print_error(void)
 {
-	write(1, "map error\n", 10);
+	write(2, "map error\n", 10);
 }
